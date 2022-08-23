ffa1b24ae7d155b399c2a97367cbc0bb25363136
Emily <episaygnane@me.com>
2022-08-23 11:18:59 -0700

    added display todos in todos java

--- todos/todos.js
+++ todos/todos.js
@@ -16,7 +16,6 @@ const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
 todoForm.addEventListener('submit', async(e) => {
-    // on submit, create a todo, reset the form, and display the todos
     e.preventDefault();
 
     const formData = new formData.get('todo');
@@ -27,20 +26,22 @@ todoForm.addEventListener('submit', async(e) => {
     displayTodos();
 });
 
-// create todo state
-
-// add async complete todo handler function
-    // call completeTodo
-    // swap out todo in array
-    // call displayTodos
-
-   
-
 async function displayTodos() {
-    // clear the container (.innerHTML = '')
-    // display the list of todos, 
-          // call render function, pass in state and complete handler function!
-          // append to .todos
+    const todos = await getTodos();
+
+    todosEl.textContent = '';
+
+    for (let todo of todos) {
+        const todoEl = renderTodo(todo);
+
+        todoEl.addEventListener('click', async() => {
+            await completeTodo(todo.id);
+
+            displayTodos();
+        });
+
+        todosEl.append(todoEl);
+    }
 }
 
 // add page load function
