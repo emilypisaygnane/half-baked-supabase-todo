128897d9129f0a73454229de1412ecafb6a28b47
Emily <episaygnane@me.com>
2022-08-23 14:51:46 -0700

    fixed typos in todos js, able to add todo item, complete it and delete it

--- todos/todos.js
+++ todos/todos.js
@@ -10,6 +10,7 @@ import { renderTodo } from '../render-utils.js';
 
 checkAuth();
 
+
 const todosEl = document.querySelector('.todos');
 const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
@@ -17,46 +18,50 @@ const deleteButton = document.querySelector('.delete-button');
 
 todoForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-
     const data = new FormData(todoForm);
-
-    const todo = data.get('todo'); 
+    const todo = data.get('todo');
 
     await createTodo(todo);
-
-    todoForm.reset();
     
+    todoForm.reset();
+
     displayTodos();
 });
 
-async function displayTodos() {
-    const todos = await getTodos();
 
-    todosEl.innerHTML = '';
 
-    for (let todo of todos) {
-        const todoList = renderTodo(todo, handleComplete);
-
-        todosEl.append(todoList);
-    }
-}
 async function handleComplete(todo) {
     await completeTodo(todo.id);
 
     displayTodos();
 }
+
+
+async function displayTodos() {
+    todosEl.innerHTML = '';
+    const todos = await getTodos();
+    for (let todo of todos) {
+        const todoList = renderTodo(todo, handleComplete);
+        todosEl.append(todoList);
+    }
+
+}
+
+
 async function onLoad() {
     await getTodos();
     displayTodos();
 }
+
 onLoad();
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
-deleteButton.addEventListener('click', async () => {
-    await deleteAllTodos();
 
-    displayTodos;
+deleteButton.addEventListener('click', async () => {
+    
+    await deleteAllTodos();
+    displayTodos();
 });
